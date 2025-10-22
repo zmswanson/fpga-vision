bbb6f10d16ffb47df41849ea210b17377b85c650
Zach Swanson <zach.swanson@doane.edu>
2025-10-22 01:19:46 -0500

    Cleaning up changes on disk.

--- hdl/sccb_interface.v
+++ hdl/sccb_interface.v
@@ -5,13 +5,14 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 module sccb_interface (
-    clk,
+    clk,                     // Basys 3 Osc = 100 MHz; MKR Vidor 400 = 48 - 200 MHz
+    reset,
     transmit,
     receive,
     address,
     write_data,
     read_data,
-    sio_c,
+    sio_c,                   // Max f_sioc = 400 kHz
     sio_d
 );
 
@@ -26,19 +27,40 @@ module sccb_interface (
 
     reg [1:0] state_reg;
 
-    parameter idle  = 2'b00;
-    parameter start = 2'b01;
-    // parameter 
+    parameter idle    = 3'b000;
+    parameter start   = 3'b001;
+    parameter addr_tx = 3'b010;
+    parameter data_tx = 3'b011;
+    parameter stop    = 3'b111;
 
-    always @ ( posedge clk ) begin
-        case (state_reg)
-            idle:
-                sio_c <= 1'b0;
-            start:
-                sio_c <= 1'b1
-            default: 
-                state_reg <= idle;
-        endcase
+    
+    // Synchronous Code Block
+    // Implement FSM for SCCB communication
+    // Assume that slave device will never send data, i.e don't worry about processing received data
+    always @ ( posedge clk or posedge reset ) begin
+        if (conditions) begin
+            state_reg <= stop;
+        end
+        else begin 
+            case (state_reg)
+                idle:
+                    sio_c <= 1'b0;
+                start:
+                    sio_c <= 1'b1;
+                addr_tx:
+                data_tx:
+                stop:
+
+                default: 
+                    state_reg <= idle;
+            endcase
+        end
+    end
+
+    // Asynchronous Code Block
+    // Consider controlling SIO_C from here
+    if (conditions) begin
+        
     end
     
 endmodule
